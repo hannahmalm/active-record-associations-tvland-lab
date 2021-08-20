fcc8cef88d0faebc5d057bea28ea7c68e24a8721
Hannah Malm <hannahemalm616@gmail.com>
2021-08-19 21:00:03 -0400

    commit tree 2b0c9383408165d5b9ff52c575791194e7bd3d2a

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters 
+
+  def full_name 
+    #list its full name - print it out
+    "#{self.first_name} #{self.last_name}"
+  end 
+
+  def list_roles 
+    characters.collect do |character|
+     "#{character.name} - #{character.show.name}"
+    
+    end 
+  end 
 end
